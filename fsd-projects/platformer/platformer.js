364867d46ce68ab3fd92260a5cf2a97f096f792c
TyZeno26 <zeno45078@students.apsb.org>
2026-02-24 14:31:53 +0000

    Finalized platformer game

--- fsd-projects/platformer/platformer.js
+++ fsd-projects/platformer/platformer.js
@@ -27,21 +27,39 @@ $(function () {
     //////////////////////////////////
 
     // TODO 1 - Enable the Grid
-    // toggleGrid();
+    ///toggleGrid();
 
 
     // TODO 2 - Create Platforms
-
+    createPlatform(300, 700, 60, 30, "red");
+    createPlatform(451, 660, 159, 20, "green");
+    createPlatform(630, 535, 70, 25, "blue");
+    createPlatform(815, 490, 20, 10, "orange");
+    createPlatform(610, 380, 30, 15, "yellow");
+    createPlatform(355, 290, 50, 10, "indigo");
+    createPlatform(935, 590, 100, 15, "violet");
+    createPlatform(1150, 645, 75, 5, "pink");
+    createPlatform(995, 390, 110, 20, "turquoise");
+    createPlatform(1210, 285, 74, 50, "brown");
 
 
 
     // TODO 3 - Create Collectables
-
+    createCollectable("database", 360, 225, 10.0, 1.0);
+    createCollectable("max", 805, 390, 0.5, 1.0);
+    createCollectable("steve", 1227, 236, 0.5, 1.0);
+    createCollectable("diamond", 1160, 545, 1.0, 1.0);
 
 
     
     // TODO 4 - Create Cannons
-
+    createCannon("bottom", 700, 700);
+    createCannon("right", 620, 2000);
+    createCannon("right", 475, 2500);
+    createCannon("top", 1300, 2550);
+    createCannon("top", 999, 3000);
+    createCannon("top", 450, 3000);
+    createCannon("left", 185, 2900, 100, 10, 100, 250, 3);
 
     
     
